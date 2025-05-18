a87261d37b029d5a6b6008e015b2f850074a19fa
Dark-Angeel <133654202+Dark-Angeel@users.noreply.github.com>
2025-05-18 18:41:27 +0200

    muzika, settings

---
+++ app/src/main/java/com/example/mobileshipbattle/MusicService.kt
@@ -0,0 +1,30 @@
+package com.example.mobileshipbattle
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MusicService : Service() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, R.raw.waves)
+        mediaPlayer.isLooping = true
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        mediaPlayer.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+}
